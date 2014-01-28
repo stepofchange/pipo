578ed6ff3aacd23e91880854dbad4acae40e431c
stepofchange <stepofchange.vlad@gmail.com>
2014-01-28 13:42:02 +0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pipo::Application.config.secret_key_base = '751f9a1093e9fe2599398d0ee22b3f497508c7c2a993d6805c93d6a0a907558a92512d967d4f3f63db91dd96292b64a4e4e593e8031b01a2aa01feb9802d6f1f'
