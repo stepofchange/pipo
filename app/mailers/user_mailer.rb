df4e37c022486fbbf04eadf063223e8aac8b9cb2
stepofchange <stepofchange.vlad@gmail.com>
2014-02-25 15:14:29 +0700

    Add user following

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user) 
   	@user = user
-    mail(:from => "stepofchange.vlad@gmail.com", :to => user.email, :subject => "Thank you for registration")
+    mail(:from => "rangeterest@gmail.com", :to => user.email, :subject => "Thank you for registration")
   end
 
 end
